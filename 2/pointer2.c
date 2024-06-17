97eba4e618efbaf67ddfebec1448f8b959e0583b
mio256 <mio256@mio256m3.local>
2024-06-17 14:27:55 +0900

    :fire: 2; pointer2.c; guess memory size

---
+++ 2/pointer2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    // 1. ポインタ変数の宣言
+    char c, *cp;
+    int i, *ip;
+    double d, *dp;
+    // 2. ポインタ変数の演算
+    cp = &c;
+    printf("cp = %p, cp+1 = %p, cp+2 = %p\n", cp, cp + 1, cp + 2);
+    // 制御文字 %p は 16進アドレス表示を指定
+    ip = &i;
+    printf("ip = %p, ip+1 = %p, ip+2 = %p\n", ip, ip + 1, ip + 2);
+    dp = &d;
+    printf("dp = %p, dp+1 = %p, dp+2 = %p\n", dp, dp + 1, dp + 2);
+}
+
+/*
+※アーキテクチャによって異なる
+
+利用可能なメモリアドレス空間サイズ 64bit
+各データ型変数のメモリサイズ
+char 1byte
+int 4byte
+double 1byte
+*/
